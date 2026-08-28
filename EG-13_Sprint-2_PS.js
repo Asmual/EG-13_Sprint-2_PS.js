e6dc8771bd26250173bd9059dc8e8f0a0c7f6d1a
Asmual Obaidul Hoque <73558650+Asmual@users.noreply.github.com>
2026-08-28 17:52:31 +0600

    added sumArray solution with test case

--- EG-13_Sprint-2_PS.js
+++ EG-13_Sprint-2_PS.js
@@ -36,3 +36,16 @@ return str === reversed;
 
 // console.log(isPalindrome("racecar"));
 // Expected Output: true
+
+
+/********** 4. Sum Array Elements **********/
+function sumArray(nums) {
+let sum = 0;
+for (let i = 0; i < nums.length; i++) {
+sum += nums[i];
+}
+return sum;
+}
+
+// console.log(sumArray([10, 20, 30, 40]));
+// Expected Output: 100
