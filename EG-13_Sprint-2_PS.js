f2b35a0a4d8fe29a4c839b6f8038aab6e4c7c801
Asmual Obaidul Hoque <73558650+Asmual@users.noreply.github.com>
2026-08-28 17:37:40 +0600

    added findMax solution with test case

--- EG-13_Sprint-2_PS.js
+++ EG-13_Sprint-2_PS.js
@@ -1,4 +1,4 @@
-// 1. Reverse a String 
+/********** 1. Reverse a String *********/
 function reverseString(str) {
     let reversed = "";
     for (let i = str.length - 1; i >= 0; i--) {
@@ -9,3 +9,17 @@ function reverseString(str) {
 
 // console.log(reverseString("hello")); 
 // Expected Output: "olleh"
+
+/********** 2. Find Maximum *********/
+function findMax(nums) {
+let max = nums[0];
+for (let i = 1; i < nums.length; i++) {
+if (nums[i] > max) {
+max = nums[i];
+}
+}
+return max;
+}
+
+// console.log(findMax([3, 7, 2, 9, 5]));
+// Expected Output: 9
