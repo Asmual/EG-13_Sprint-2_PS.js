9640bce53ceb5599fcea6c43f58f88ed0dfbccde
Asmual Obaidul Hoque <73558650+Asmual@users.noreply.github.com>
2026-08-28 17:50:00 +0600

    added isPalindrome solution with test case

--- EG-13_Sprint-2_PS.js
+++ EG-13_Sprint-2_PS.js
@@ -23,3 +23,16 @@ return max;
 
 // console.log(findMax([3, 7, 2, 9, 5]));
 // Expected Output: 9
+
+
+/********** 3. Check for Palindrome **********/
+function isPalindrome(str) {
+let reversed = "";
+for (let i = str.length - 1; i >= 0; i--) {
+reversed += str[i];
+}
+return str === reversed;
+}
+
+// console.log(isPalindrome("racecar"));
+// Expected Output: true
