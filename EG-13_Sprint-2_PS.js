f106fe9faa9ae571003dbe335907b6ad8ed50a4d
Asmual Obaidul Hoque <73558650+Asmual@users.noreply.github.com>
2026-08-28 17:54:07 +0600

    added flattenArray solution with test case

--- EG-13_Sprint-2_PS.js
+++ EG-13_Sprint-2_PS.js
@@ -81,3 +81,18 @@ return [];
 // Expected Output: [0, 1]
 
 
+/********** 7. Flatten a Nested Array **********/
+function flattenArray(arr) {
+let result = [];
+for (let i = 0; i < arr.length; i++) {
+if (Array.isArray(arr[i])) {
+result = result.concat(flattenArray(arr[i]));
+} else {
+result.push(arr[i]);
+}
+}
+return result;
+}
+
+// console.log(flattenArray([1, [2, [3, 4], 5]]));
+// Expected Output: [1, 2, 3, 4, 5]
